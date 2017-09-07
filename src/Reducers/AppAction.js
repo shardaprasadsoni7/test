6667e9cce8feb08206fa9de7687bba8ba386dd3e
Sharda Prasad <sharda.prasad@ucertify.com>
2017-09-07 15:23:30 +0530

    rsolve login break

--- src/Reducers/AppAction.js
+++ src/Reducers/AppAction.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase';
 
-function isEmail (email) {
+export function isEmail (email) {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
 }
